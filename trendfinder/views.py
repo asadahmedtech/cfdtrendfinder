528096995e1c737c080020a3a3ec1e3e373cc437
asad <ahmed.asadtech@gmail.com>
2017-11-09 00:12:31 +0530

    initial

--- trendfinder/views.py
+++ trendfinder/views.py
@@ -6,14 +6,14 @@ from . import twitterAnalysis
 # Create your views here.
 def get_trend_from_twitter(keyword):
 	tweets,pos,neg = twitterAnalysis.getRes(keyword)
-	data = [tweets[0],tweets[1],tweets[2],pos,neg]
-	return data
+	data,sentiment = [tweets[0],tweets[1],tweets[2]],['Negative Sentiment is '+str(pos),'Positive Sentiment is '+str(neg)]
+	return (data,sentiment)
 def index(request):
 	return render(request, 'trendfinder/index.html')
 
 def gettrend(request):
 	if request.method == 'POST':
 		tweet = request.POST.get('keyword')
-		trend_data = get_trend_from_twitter(tweet)
-		return render(request, 'trendfinder/show.html', {'data':trend_data})
+		trend_data,sentiment = get_trend_from_twitter(tweet)
+		return render(request, 'trendfinder/show.html', {'data':trend_data,'sentiment':sentiment,'keyword':tweet})
 	return render(request, 'trendfinder/index.html')
